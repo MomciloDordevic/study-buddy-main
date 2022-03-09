61ba101211e151e0c4cf79f655b3787eb754dac3
Momcilo <qwertyumom@gmail.com>
2022-03-09 15:52:17 +0100

    Solved the issue with images, trying to make the functionality behind selecting an avatar image

--- src/Components/UI/AvatarModal.js
+++ src/Components/UI/AvatarModal.js
@@ -1,31 +1,67 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
-import image from '../../Assets/Avatars/1.png';
+import image1 from "../../Assets/Avatars/1.png";
+import image2 from "../../Assets/Avatars/2.png";
+import image3 from "../../Assets/Avatars/3.png";
+import image4 from "../../Assets/Avatars/4.png";
+import image5 from "../../Assets/Avatars/5.png";
+import image6 from "../../Assets/Avatars/6.png";
+import image7 from "../../Assets/Avatars/7.png";
+import image8 from "../../Assets/Avatars/8.png";
 import classes from "./AvatarModal.module.css";
+
+const imageList = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+];
 
 const Backdrop = (props) => {
   return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
+  const [selectedAvatar, setSelectedAvatar] = useState(false);
+
+  const showSelectedAvatar = () => {
+    setSelectedAvatar(true)
+  }
+
+  const hideSelectedAvatar = () => {
+    setSelectedAvatar(false);
+  }
+
+
   return (
     <div className={classes.modal}>
       <h2>Pick Your Studdy Buddy</h2>
       <div>{props.children}</div>
-        <div className={classes.content}>
-          <img src={image} width="130" height="120" alt="Testing" />
-          <img src="" width="130" height="120" alt="Testing"></img>
-          <img src="./1.png" width="130" height="120" alt="Testing"></img>
-          <img src="Assets/Avatars/1.png" width="130" height="120" alt="Testing" />
-          <img src="http://localhost:3577/static/media/1.d31d459d.png" width="130" height="120" alt="Testing" />
-          <img src="src/Assets/Avatars/1.png" width="130" height="120" alt="Testing" />
-          <img src="../../Assets/Avatars/1.png" width="130" height="120" alt="Testing" />
-          <img src="/Assets/Avatars/1.png" width="130" height="120" alt="Testing"></img>
-          <div>
-            <button onClick={props.onClose}>Close</button>
-          </div>
-        </div>
+      <div className={classes.content}>
+        {imageList.map((each, i) => (
+          <img
+            key={i}
+            src={each}
+            width="140"
+            height="120"
+            alt=""
+            onClick={showSelectedAvatar}
+          />
+        ))}
+        {/* <img src={image1} width="140" height="120" alt="" />
+          <img src={image2} width="140" height="120" alt="" />
+          <img src={image3} width="140" height="120" alt="" />
+          <img src={image4} width="140" height="120" alt="" />
+          <img src={image5} width="140" height="120" alt="" /> */}
+      </div>
+      <div>
+        <button onClick={props.onClose}>Close</button>
+      </div>
     </div>
   );
 };
